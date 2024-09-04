eaf81302b9ee40e125e07b26ff85875aecbbf22e
owner <Andro>
2024-09-04 05:13:19 -0400

    Finalization of seo functionality

--- app/Models/Admin/LTSPSEO.php
+++ app/Models/Admin/LTSPSEO.php
@@ -2,8 +2,9 @@
 
 namespace App\Models\Admin;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Tags\HasTags;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class LtspSeo extends Model
 {
@@ -12,6 +13,6 @@ class LtspSeo extends Model
         "meta_title",
         "meta_description",
         "is_redirection",
-        
-    ]
+        "category_id"
+    ];
 }
