d41b9d8443c5644d668b050e72613cb46da4b3ea
Andro <androbienaime@gmail.com>
2024-10-20 16:56:54 -0700

    re-design ShopResource

--- app/Filament/Resources/Core/ShopResource.php
+++ app/Filament/Resources/Core/ShopResource.php
@@ -2,16 +2,21 @@
 
 namespace App\Filament\Resources\Core;
 
-use App\Filament\Resources\Core\ShopResource\Pages;
-use App\Filament\Resources\Core\ShopResource\RelationManagers;
-use App\Models\Core\Shop;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
+use App\Models\Core\Shop;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\Core\ShopResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use App\Filament\Resources\Core\ShopResource\RelationManagers;
 
 class ShopResource extends Resource
 {
@@ -31,17 +36,83 @@ class ShopResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('reference')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('theme_name')
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('theme_color')
-                    ->maxLength(255),
-                Forms\Components\Toggle::make('status')
-                    ->required(),
+                Section::make()
+                    ->schema([
+                        SpatieMediaLibraryFileUpload::make('shop_cover')
+                                    ->imageEditor()
+                                    ->image()
+                                    ->collection('shop_cover')
+                                    ->responsiveImages()
+                                    ->conversion('thumb')
+                                    ->optimize('webp')
+                                    ->columnSpan('full')
+                                    ->downloadable()
+                                    // ->imagePreviewHeight(150)
+                                    ->panelLayout("compact")
+                                    ,
+                        SpatieMediaLibraryFileUpload::make('shop_profile')
+                                ->avatar()
+                                ->collection('shop_profile')
+                                ->label("")
+                                ->imageEditor()
+                                ->circleCropper()
+                                ->responsiveImages()
+                                ->conversion('thumb')
+                                ->optimize('webp')
+                                ->columnSpan('full')
+                                ->downloadable()
+                                ->panelAspectRatio('8:8')
+                                // ->imagePreviewHeight(150)
+                                ->panelLayout("circle")
+                                ->alignCenter()
+                                ,
+                    ])->extraAttributes(['class' => 'custom-section']),
+
+                Grid::make(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live()
+                            ->afterStateUpdated(function (Get $get, Set $set) {
+                                $set('slug', Shop::createUniqueSlug($get('name')));
+                            }),
+                        Forms\Components\TextInput::make('slug')
+                            ->live()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\Select::make('types')
+                            ->options([
+                                "Store" => "Store",
+                                "Dry" => "Dry",
+                                "Market" => "Market",
+                                "Restaurant" => "Restaurant",
+                                "Bar" => "Bar",
+                                "Cafe" => "Cafe",
+                                "Other" => "Other",
+                            ])
+                            ->searchable()
+                            ->preload(),
+                        Forms\Components\TextInput::make('reference')
+                            ->maxLength(255),
+                        Forms\Components\RichEditor::make('shop_description')
+                            ->maxLength(255)
+                            ->toolbarButtons([
+                                'bold',
+                                'italic',
+                                'link',
+                                'redo',
+                                'underline',
+                                'undo',])
+                            ->ColumnSpan("full"),
+
+                        Forms\Components\TextInput::make('theme_name')
+                            ->maxLength(255),
+                        Forms\Components\ColorPicker::make('theme_color'),
+                        Forms\Components\Toggle::make('status')
+                            ->required(),
+
+                    ])
             ]);
     }
 
